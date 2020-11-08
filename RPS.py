837b07f145a2298073ce41c4ef030940d6e6ce0e
Creamy_Chris <marius.cristian.capatina@gmail.com>
2020-11-08 14:51:45 +0200

    MaCa update #1

--- RPS.py
+++ RPS.py
@@ -1,77 +1,82 @@
+import random
 from random import randint
 import time
 
+thisdict = {  
+  "r":"Rock",
+  "p":"Paper",
+  "s":"Scissors"
+}
+rules_loose = {
+    "Scissors Rock": "Rock blunts Scissors, you loose",
+    "Paper Scissors": "Scissors cuts Paper, you loose",        
+    "Rock Paper": "Paper covers Rock, you loose"
+}
+rules_win = {
+    "Rock Scissors": "Rock blunts Scissors, you win",
+    "Scissors Paper": "Scissors cuts Paper, you win",
+    "Paper Rock": "Paper covers Rock, you win"
+}
+
+def print_choices():
+    #Player is chosing a character.  
+    for x in thisdict:
+        print(thisdict[x], end =" ")
+        print("(", end =" ")
+        print(x, end =" ")
+        print("),", end =" ")
 #set number of rounds
+def main():
+    print ('Welcome to my game of', end =" ")
+    for x in thisdict:
+      print(thisdict[x], end =" ")
+    print ('!!!')
+    time.sleep (1)
+    rounds = int(input ('Best out of ...? (1 to 10)'))
+    if rounds > 10:
+        rounds = 10
+    if rounds == 0:
+        return
+    print ('Let us play', rounds, 'rounds')
 
-print ('Welcome to my game of Rock, Paper, Scissors!!!')
-time.sleep (1)
-rounds = int(input ('Best out of ...? (1 to 10)'))
-print ('Let us play', rounds, 'rounds')
+    #main Game Loop
+    player_score = 0
+    cpu_score =0
+    keep_going = 0    
+    while keep_going < rounds:                
+        is_in = False
+        while is_in == False:            
+            print_choices()
+            playerC = input()
+            is_in = playerC in thisdict
+        player = thisdict[playerC]    
+        print (player, 'vs', end= ' ')
 
+        #Computer is chosing an item.
+        computer = random.choice(list(thisdict.values()))    
+        print (computer)
 
+        # Compare the two and define the winner
+        fight = player + ' ' + computer        
+        if computer == player:
+            print (player, 'likes', computer, "-it's a draw!!!")
+        elif fight in rules_loose:
+            print(rules_loose[fight])
+            cpu_score += 1
+        elif fight in rules_win:
+            print(rules_win[fight])
+            player_score += 1
+        #Count the winners
+        print (player_score, 'vs', cpu_score)
+        keep_going += 1
+    if keep_going == rounds:
+        if cpu_score > player_score:
+            print ('You lose... :(')
+        elif cpu_score == player_score:
+            print ("It's a DRAW!!!")
+        else :
+            print ('You win...:)')
+        break
 
-#main Game Loop
-player_score = 0
-cpu_score =0
-keep_going = 0
-while keep_going < rounds:
-    
-    #Player is chosing a character.       
-    playerC = input('Rock (r), Paper (p), Scissors (s)')
-    if playerC == ('r'):
-        player = ('Rock')
-    elif playerC == ('s'):
-        player = ('Scissors')
-    elif playerC == ('p'):
-        player = ('Paper')
-    else:
-        player = ("That's not a choice")
-    print (player, 'vs', end= ' ')
-
-    #Computer is chosing a character.
-
-    c_choice = randint (1,3)
-    if c_choice == (1):
-        computer = ('Rock')
-    elif c_choice == (2):
-        computer = ('Paper')
-    elif c_choice == (3):
-        computer = ('Scissors')
-    print (computer)
-
-    # Compare the two and define the winner
-    
-    if computer == player:
-        print (player, 'likes', computer, "-it's a draw!!!")
-    elif computer == ('Rock') and player == ('Scissors'):
-        print (computer, 'blunts', player, ', you loose.')
-        cpu_score += 1
-    elif computer == ('Scissors') and player == ('Paper'):
-        print (computer, 'cuts', player, ', you loose.')
-        cpu_score += 1
-    elif computer == ('Paper') and player == ('Rock'):
-        print (computer, 'covers', player, ', you loose.')
-        cpu_score += 1
-    elif player == ('Rock') and computer == ('Scissors'):
-        print (player, 'blunts', computer, ', you win.')
-        player_score += 1
-    elif player == ('Scissors') and computer == ('Paper'):
-        print (player, 'cuts', computer, ', you win.')
-        player_score += 1
-    elif player == ('Paper') and computer == ('Rock'):
-        print (player, 'covers', computer, ', you win.')
-        player_score += 1
-    #Count the winners
-    print (player_score, 'vs', cpu_score)
-    keep_going += 1
-while keep_going == rounds:
-    if cpu_score > player_score:
-        print ('You lose... :(')
-    elif cpu_score == player_score:
-        print ("It's a DRAW!!!")
-    else :
-        print ('You win...:)')
-    break
-
-
-
+if __name__ == "__main__":
+    main()
