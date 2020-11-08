8eba4d8c3fa061dc1203e2889de50afb5edde6dc
Creamy_Chris <marius.cristian.capatina@gmail.com>
2020-11-08 15:36:09 +0200

    update to RPSLS rules, other improvements

--- RPS.py
+++ RPS.py
@@ -2,24 +2,28 @@ import random
 from random import randint
 import time
 
-thisdict = {  
+thisdict = {
   "r":"Rock",
   "p":"Paper",
-  "s":"Scissors"
+  "s":"Scissors",
+  "l": "Lizard",
+  "k": "Spock"
 }
-rules_loose = {
-    "Scissors Rock": "Rock blunts Scissors, you loose",
-    "Paper Scissors": "Scissors cuts Paper, you loose",        
-    "Rock Paper": "Paper covers Rock, you loose"
-}
-rules_win = {
-    "Rock Scissors": "Rock blunts Scissors, you win",
-    "Scissors Paper": "Scissors cuts Paper, you win",
-    "Paper Rock": "Paper covers Rock, you win"
+rules = {
+    "Spock Rock": "Spock vaporizes Rock",
+    "Lizard Paper": "Lizard eats Paper",
+    "Lizard Spock": "Lizard poisons Spock",
+    "Paper Rock": "Paper covers Rock",
+    "Paper Spock": "Paper disproves Spock",
+    "Rock Lizard": "Rock crushes Lizard",
+    "Rock Scissors": "Rock crushes Scissors",
+    "Scissors Paper": "Scissors cut Paper",
+    "Scissors Lizard": "Scissors decapitate Lizard",
+    "Spock Scissors": "Spock smashes Scissors"
 }
 
 def print_choices():
-    #Player is chosing a character.  
+    #Player is chosing a character.
     for x in thisdict:
         print(thisdict[x], end =" ")
         print("(", end =" ")
@@ -29,10 +33,14 @@ def print_choices():
 def main():
     print ('Welcome to my game of', end =" ")
     for x in thisdict:
-      print(thisdict[x], end =" ")
+      print(thisdict[x], end ="-")
     print ('!!!')
     time.sleep (1)
-    rounds = int(input ('Best out of ...? (1 to 10)'))
+    player_request = input ('Best out of ...? (1 to 10)')
+    if player_request.isnumeric():
+        rounds = int(player_request)
+    else:
+        rounds = 3;
     if rounds > 10:
         rounds = 10
     if rounds == 0:
@@ -42,41 +50,44 @@ def main():
     #main Game Loop
     player_score = 0
     cpu_score =0
-    keep_going = 0    
-    while keep_going < rounds:                
+    keep_going = 0
+    while keep_going < rounds:
         is_in = False
-        while is_in == False:            
+        while is_in == False:
             print_choices()
             playerC = input()
+            playerC = playerC.lower()
             is_in = playerC in thisdict
-        player = thisdict[playerC]    
+        player = thisdict[playerC]
         print (player, 'vs', end= ' ')
 
         #Computer is chosing an item.
-        computer = random.choice(list(thisdict.values()))    
+        computer = random.choice(list(thisdict.values()))
         print (computer)
 
         # Compare the two and define the winner
-        fight = player + ' ' + computer        
+        fight = player + ' ' + computer
         if computer == player:
             print (player, 'likes', computer, "-it's a draw!!!")
-        elif fight in rules_loose:
-            print(rules_loose[fight])
-            cpu_score += 1
-        elif fight in rules_win:
-            print(rules_win[fight])
+        elif fight in rules:
+            print(rules[fight], ", you win!")
             player_score += 1
+        else:
+            fight = computer + ' ' + player
+            if fight in rules:
+                print(rules[fight], ", you loose!")
+                cpu_score += 1
+            else: print (player, 'likes', computer, "-it's a draw!!!")
         #Count the winners
         print (player_score, 'vs', cpu_score)
         keep_going += 1
-    if keep_going == rounds:
-        if cpu_score > player_score:
-            print ('You lose... :(')
-        elif cpu_score == player_score:
-            print ("It's a DRAW!!!")
-        else :
-            print ('You win...:)')
-        break
+
+    if cpu_score > player_score:
+        print ('You lose... :(')
+    elif cpu_score == player_score:
+        print ("It's a DRAW!!!")
+    else :
+        print ('You win...:)')
 
 if __name__ == "__main__":
     main()
